0d96360210190642b136c1ca1aaf168c56b39bf8
emilyesc <97820298+emilyesc@users.noreply.github.com>
2023-05-27 11:35:35 -0700

    Update scripture.cs

--- prove2/scripture.cs
+++ prove2/scripture.cs
@@ -1,3 +1,10 @@
 using System;
 
 public class Scripture
+
+    public List<Scripture> _scripture = new List<Scripture>();
+    private string _fileName = "referencetext.txt";
+    private string _key;
+    private string _text;
+    public int _index;
+    public string _scriptureText;
