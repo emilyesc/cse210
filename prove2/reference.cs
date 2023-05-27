5388881382442acb338438462b31f59dc5e3fa4b
emilyesc <97820298+emilyesc@users.noreply.github.com>
2023-05-27 11:29:05 -0700

    Update reference.cs

--- prove2/reference.cs
+++ prove2/reference.cs
@@ -10,3 +10,22 @@ public class Reference
     private int _verseStart;
     private int _verseEnd;
 
+  public void LoadReference()
+    {
+        List<string> readText = File.ReadAllLines(_fileName).Where(arg => !string.IsNullOrWhiteSpace(arg)).ToList();
+
+        foreach (string line in readText)
+        {
+            string[] entries = line.Split(";");
+
+            Reference entry = new Reference();
+
+            entry._key = entries[0];
+            entry._book = entries[1];
+            entry._chapter = int.Parse(entries[2]);
+            entry._verseStart = int.Parse(entries[3]);
+            entry._verseEnd = int.Parse(entries[4]);
+
+            _reference.Add(entry);
+        }
+    }
