18ccffb2abf1fc20d45c941c4330b10aa7aacfe8
emilyesc <97820298+emilyesc@users.noreply.github.com>
2023-05-27 11:19:56 -0700

    Update reference.cs

--- prove2/reference.cs
+++ prove2/reference.cs
@@ -1,1 +1,12 @@
+using System;
+
+public class Reference
+{
+    public List<Reference> _reference = new List<Reference>();
+    private string _fileName = "referencetext.txt";
+    private string _key;
+    private string _book;
+    private int _chapter;
+    private int _verseStart;
+    private int _verseEnd;
 
