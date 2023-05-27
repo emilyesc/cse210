f3c7e84387f172a3a7c0cfe27795022b676a3b2d
emilyesc <97820298+emilyesc@users.noreply.github.com>
2023-05-27 11:12:15 -0700

    Update program.cs

--- prove2/program.cs
+++ prove2/program.cs
@@ -13,7 +13,43 @@ class Program
         Word word = new Word();
         
      Console.Write("\n**** Welcome to the Scripture Memorizer Program ****\n");
-     
+    
+        int userChoice = 0;
+
+        while (userChoice != 3)
+        {
+            userChoice = UserChoice();
+
+            switch (userChoice)
+            {
+                case 1:
+                    reference.ReferenceDisplay();
+
+                    break;
+                case 2:
+                    string script = scripture.RandomScripture();
+                    string ref1 = reference.GetReference(scripture);
+                    word.GetRenderedText(scripture);
+                    word.GetRenderedRef(scripture);
+                  
+
+                    while (word._hidden.Count < word._result.Length)
+                    {
+                        word.Show(ref1);
+                        word.GetReadKey();
+                    }
+                    word.Show(ref1);
+                    break;
+                case 3:
+                    Console.WriteLine("\n*** Thanks for playing! ***\n");
+                    break;
+                default:
+                    Console.WriteLine($"\nSorry the option you entered is not valid.");
+                    break;
+            }
+        }
+
+    }
      
     static int UserChoice()
     // Method to display choices to user
