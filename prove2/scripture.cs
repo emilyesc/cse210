5455082b2ff98f581ce04237f8cb1d27f0a17f60
emilyesc <97820298+emilyesc@users.noreply.github.com>
2023-05-27 11:37:11 -0700

    Update scripture.cs

--- prove2/scripture.cs
+++ prove2/scripture.cs
@@ -8,3 +8,20 @@ public class Scripture
     private string _text;
     public int _index;
     public string _scriptureText;
+
+public void LoadScriptures()
+    {
+        List<string> readText = File.ReadAllLines(_fileName).Where(arg => !string.IsNullOrWhiteSpace(arg)).ToList();
+
+        foreach (string line in readText)
+        {
+            string[] entries = line.Split(";");
+
+            Scripture entry = new Scripture();
+
+            entry._key = entries[0];
+            entry._text = entries[6];
+
+            _scripture.Add(entry);
+        }
+    }
