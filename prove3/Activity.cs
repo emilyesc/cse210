657408a44ce002eb5aef1da8bb35b4d3af83f647
emilyesc <97820298+emilyesc@users.noreply.github.com>
2023-06-10 16:06:13 -0700

    Update Activity.cs

--- prove3/Activity.cs
+++ prove3/Activity.cs
@@ -1,7 +1,8 @@
 using System;
 
 class Activity
-{
+{   
+    //this makes it so the welcome and end messages can be reused through inheritance
     protected string _welcomeMessage = "";
     protected string _endingMessage = "";
 
