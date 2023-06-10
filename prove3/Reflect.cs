379ca950774574a4298d3e391c91a12d0afccf5b
emilyesc <97820298+emilyesc@users.noreply.github.com>
2023-06-10 16:53:24 -0700

    Update Reflect.cs

--- prove3/Reflect.cs
+++ prove3/Reflect.cs
@@ -40,7 +40,47 @@ class Reflecting : Activity
     {
 
     }
+    
+    
+    List<string> startQuestions = new List<string>()
+    {
+        "> Think of a time when you stood up for someone.\n \n",
+        "> Think of a time when you did something really difficult.\n \n",
+        "> Think of a time when you helped someone in need.\n \n",
+        "> Think of a time when you did something truly selfless.\n \n",
+        "> Think of a time when you followed the promptings of the Holy Ghost.\n \n",
+        "> Think of something amazing that happened recently that you want to remember.\n \n",
+        "> Think of a time you made someone smile. \n \n",
+        "> Think of a time when the work you were doing felt satisfying. \n \n"
+    };
 
+   public void startReflecting()
+    {
+        Console.WriteLine(_welcomeMessage);
+        Timer refTime = new Timer();
+        refTime.activityTime();
+        int seconds = refTime._timeSet;
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(refTime._timeSet);
+        if (DateTime.Now < endTime)
+        {
+            var random = new Random();
+            int index = random.Next(startQuestions.Count);
+            string startQ = startQuestions[index];
+            Console.WriteLine(startQ);
+            Thread.Sleep(10000);
+            while (DateTime.Now < endTime)
+            {
+                var random2 = new Random();
+                int index2 = random.Next(refQuestions.Count);
+                string refQ = refQuestions[index2];
+                refQuestions.Remove(refQuestions[index2]);
+                Console.WriteLine(refQ);
+                Thread.Sleep(10000);
+
+            }
+        }
+        
     Console.WriteLine($"Well done! You completed {seconds} seconds of the reflecting activity.\n");
         Thread.Sleep(5000);
     }
