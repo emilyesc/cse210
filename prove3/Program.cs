45d961d5de6ef12b14b938c9d57faa340bb41e41
emilyesc <97820298+emilyesc@users.noreply.github.com>
2023-06-10 14:48:33 -0700

    Update Program.cs

--- prove3/Program.cs
+++ prove3/Program.cs
@@ -8,6 +8,11 @@ class Program
 
           while (runProg == true)
           {
+            Menu menu = new Menu();
+            Console.WriteLine("\nWelcome to the Mindfulness Program. Each activity is designed to help you \nslow down from the fast-paced world, and to think and feel deeply.\nPlease select from the following activities, or select 4 to quit.\n");
+            menu.DisplayMenu();
+            int activity = Int32.Parse(Console.ReadLine());
+
           }
     }
 }
