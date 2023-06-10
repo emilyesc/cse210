b418827139058e5f3d7aa8da79cce4f94482362f
emilyesc <97820298+emilyesc@users.noreply.github.com>
2023-06-10 14:59:03 -0700

    Update Program.cs

--- prove3/Program.cs
+++ prove3/Program.cs
@@ -12,6 +12,44 @@ class Program
             Console.WriteLine("\nWelcome to the Mindfulness Program. Each activity is designed to help you \nslow down from the fast-paced world, and to think and feel deeply.\nPlease select from the following activities, or select 4 to quit.\n");
             menu.DisplayMenu();
             int activity = Int32.Parse(Console.ReadLine());
+              
+             if (activity == 1)
+            {
+                Timer startTime = new Timer();
+                startTime.prepTime();
+                Breathing breathing = new Breathing();
+                breathing.startBreathing();
+
+            }
+              
+            else if (activity == 2)
+            {
+                Timer startTime = new Timer();
+                startTime.prepTime();
+                Reflecting reflecting = new Reflecting();
+                reflecting.startReflecting();
+
+            }
+              
+            else if (activity == 3)
+            {
+                Timer startTime = new Timer();
+                startTime.prepTime();
+                Listing listing = new Listing();
+                listing.startListing();
+            }
+              
+            else if (activity == 4)
+            {
+            runProg = false;
+            }
+              
+            else
+            {
+                Console.WriteLine("Sorry, you did not input a valid option. Please try again.");
+                
+            }
+
 
           }
     }
