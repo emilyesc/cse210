d8544ff5419ce0ebb28e8af1f704fa1b3ef100a9
emilyesc <97820298+emilyesc@users.noreply.github.com>
2023-06-10 16:41:23 -0700

    Create Reflect.cs

---
+++ prove3/Reflect.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Reflecting : Activity
+{
+    string _welcomeMessage = "\nThis activity will help you reflect on times in your life when you have shown \nstrength and resilience. This will help you recognize the power you have and how you can use it \nin other aspects of your life.\n \n";
+
+    };
+    
+    public Reflecting()
+    {
+
+    }
+
+    Console.WriteLine($"Well done! You completed {seconds} seconds of the reflecting activity.\n");
+        Thread.Sleep(5000);
+    }
+
+}
