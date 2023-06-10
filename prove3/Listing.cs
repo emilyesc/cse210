3eb91f598850b50357e7af696e6922dcce5e8732
emilyesc <97820298+emilyesc@users.noreply.github.com>
2023-06-10 16:13:28 -0700

    Create Listing.cs

---
+++ prove3/Listing.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Listing : Activity
+{
+    public Listing()
+    {
+
+    }
+
+    List<string> keepListing = new List<string>()
+    {
+        "> Who are people that you appreciate? \n",
+        "> What are personal strengths of yours? \n",
+        "> Who are people that you have helped this week? \n",
+        "> When have you felt the Holy Ghost this month? \n",
+        "> Who are some of your personal heroes? \n",
+        "> What are some tender mercies from the Lord you've noticed? \n",
+        "> How have your prayers been answered, big or small? \n",
+        "> What are the little things you've taken time to do that have added to your days? \n",
+        "> What would you like to say yes to in the future? \n",
+        "> What would your Heavenly Father like you to remember? \n"
+    };
+    string _welcomeMessage = "\nThis activity will help you reflect on the good things in your life by having you list as many\nthings as you can in a certain area. \n";
