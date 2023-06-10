61dd55334f20e75429deaad862ff4f0e6633d282
emilyesc <97820298+emilyesc@users.noreply.github.com>
2023-06-10 16:37:23 -0700

    Create Menu.cs

---
+++ prove3/Menu.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Menu
+{
+    protected List<string> menuOpts = new List<string>()
+    {
+        "1. Breathing Activity",
+        "2. Reflection Activity",
+        "3. Listing Activity",
+        "4. Quit"
+    }
+ }
