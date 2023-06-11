40d885979dcdf6932ed6d43596197622d4243587
emilyesc <97820298+emilyesc@users.noreply.github.com>
2023-06-10 18:54:41 -0700

    Update Program.cs

--- prove3/Program.cs
+++ prove3/Program.cs
@@ -9,10 +9,11 @@ class Program
           while (runProg == true)
           {
             Menu menu = new Menu();
-            Console.WriteLine("\nWelcome to the Mindfulness Program. Each activity is designed to help you \nslow down from the fast-paced world, and to think and feel deeply.\nPlease select from the following activities, or select 4 to quit.\n");
+            Console.WriteLine("\nWelcome to the Mindfulness Program. Each activity is designed to help you \nslow down from the fast-paced world, and to think and feel deeply.\nPlease select from the following activities, or select 5 to quit.\n");
             menu.DisplayMenu();
             var activity = 0;
             
+            //used this to catch any non-integer answers
             while (!int.TryParse(Console.ReadLine(), out activity))
             {
                 Console.WriteLine("Sorry, only numerical options are accepted. Please try again.");
@@ -47,10 +48,23 @@ class Program
             }
               
             else if (activity == 4)
+                {
+                Console.WriteLine("Write one or two sentences about your day");
+                Console.ReadLine();
+            }
+                
+              //little easter egg
+            else if (activity == 911)
+            {
+                Console.WriteLine("If suffering from serious injury or pain, please use a phone to dial 911.");
+            }
+              
+            else if (activity == 5)
             {
             runProg = false;
             }
-              
+            
+              // this catches integer inputs that don't match the options
             else
             {
                 Console.WriteLine("Sorry, you did not input a valid option. Please try again.");
