01e46352b380f8dd60d9991a2e57cbb6c175d499
emilyesc <97820298+emilyesc@users.noreply.github.com>
2023-06-10 18:58:49 -0700

    Update Menu.cs

--- prove3/Menu.cs
+++ prove3/Menu.cs
@@ -1,13 +1,15 @@
 using System;
 
 public class Menu
-{
+{      
+    //protects menu list while displaying the menu
     protected List<string> menuOpts = new List<string>()
     {
         "1. Breathing Activity",
         "2. Reflection Activity",
         "3. Listing Activity",
-        "4. Quit"
+        "4. Journal Activity",
+        "5. Quit"
     }
     
     public void DisplayMenu()
