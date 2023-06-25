355430816ce337f0449d0b2610e52ac91c46d469
emilyesc <97820298+emilyesc@users.noreply.github.com>
2023-06-24 20:17:01 -0700

    Update Checklist.cs

--- prove4/Checklist.cs
+++ prove4/Checklist.cs
@@ -1,8 +1,37 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 
-public class Checklist : Goal
+class Checklist : Goal
 {
-	public Checklist()
-	{
-	}
+    // int x = 0;
+    bool _isComplete = false;
+    int _numEntries = 0;
+    int _numTillDone = 0;
+
+    public Checklist()
+    {
+        _type = "C";
+    }
+    public Checklist(string name, string desc, int points, int numTillDone, int numEntries)
+    {
+        _type = "C";
+        _name = name;
+        _description = desc;
+        _pointVal = points;
+        _numTillDone = numTillDone;
+        _numEntries = numEntries;
+
+    }
+
+
+    public int promptEntries()
+    {
+        Console.WriteLine("\nHow many times until this goal is complete? ");
+        int numTillDone = Int32.Parse(Console.ReadLine());
+        _numTillDone = numTillDone;
+        return _numTillDone;
+
+    }
+
 }
