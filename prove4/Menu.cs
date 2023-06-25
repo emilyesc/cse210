dd346f969a25663ff43ac65b0ff4de86669e5380
emilyesc <97820298+emilyesc@users.noreply.github.com>
2023-06-24 20:16:00 -0700

    Uploaded correct Menu.cs

--- prove4/Menu.cs
+++ prove4/Menu.cs
@@ -1,1 +1,46 @@
 
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+public class Menu
+{
+    public Menu()
+    {
+
+    }
+    List<string> menuOpts = new List<string>
+    {
+        "1. Create a Goal",
+        "2. List Goals",
+        "3. Save Goals",
+        "4. Load Goals",
+        "5. Record Events",
+        "6. Quit"
+    };
+
+    public void displayMenu()
+    {
+        foreach (string opts in menuOpts)
+        {
+            Console.WriteLine(opts);
+        }
+    }
+
+    List<string> goalType = new List<string>
+    {
+        "1. Simple",
+        "2. Checklist",
+        "3. Eternal"
+    };
+
+    public void displayGoalTypes()
+    {
+        foreach (string type in goalType)
+        {
+            Console.WriteLine(type);
+        }
+    }
+
+}
