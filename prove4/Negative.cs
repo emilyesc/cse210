97f06b4474762480cb5f1911cef16f5da68fac27
emilyesc <97820298+emilyesc@users.noreply.github.com>
2023-06-24 20:36:37 -0700

    Create Negative.cs

---
+++ prove4/Negative.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Negative : Goal
+{
+    bool _isComplete = false;
+
+    public Negative()
+    {
+        _type = "N";
+
+    }
+
+    public Negative(string name, string description, int pointVal, bool isComplete)
+    {
+        _type = "S";
+        _name = name;
+        _description = description;
+        _pointVal = pointVal;
+        _isComplete = isComplete;
+
+    }
