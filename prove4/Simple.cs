19102d9fa204df8caefd74448b2a6edb3d0adf8f
emilyesc <97820298+emilyesc@users.noreply.github.com>
2023-06-24 20:18:30 -0700

    Update Simple.cs

--- prove4/Simple.cs
+++ prove4/Simple.cs
@@ -1,8 +1,50 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 
-public class Simple : Goal
+class Simple : Goal
 {
-	public Simple()
-	{
-	}
+    bool _isComplete = false;
+
+    public Simple()
+    {
+        _type = "S";
+
+    }
+
+    public Simple(string name, string description, int pointVal, bool isComplete)
+    {
+        _type = "S";
+        _name = name;
+        _description = description;
+        _pointVal = pointVal;
+        _isComplete = isComplete;
+
+    }
+    public string getName()
+    {
+        Console.WriteLine("\nWhat is the name of your goal? ");
+        string name = Console.ReadLine();
+        _name = name;
+        return _name;
+    }
+
+    public string getDesc()
+    {
+        Console.WriteLine("\nWhat is a short description of your goal? ");
+        string desc = Console.ReadLine();
+        _description = desc;
+        return _description;
+    }
+
+
+    public int getPoints()
+    {
+        Console.WriteLine("\nHow many points is this goal worth? ");
+        int points = Int32.Parse(Console.ReadLine());
+        _pointVal = points;
+        return _pointVal;
+    }
+
+
 }
