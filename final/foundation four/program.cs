3ee30f71e07ffd19851ca57fe46cf35f2358860a
emilyesc <97820298+emilyesc@users.noreply.github.com>
2023-07-08 18:03:40 -0700

    Create program.cs

---
+++ final/foundation four/program.cs
@@ -0,0 +1,10 @@
+namespace foundation4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the fourth program");
+        }
+    }
+}
